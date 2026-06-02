03d510bbef4f10494ee38742ecf7b7db1eab1017
ht45hamza <ht45hamzatariq@gmail.com>
2026-06-02 18:16:20 +0500

    Frontend env added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load all environment variables (including those without VITE_ prefix)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: `http://localhost:${env.PORT }`,
+          changeOrigin: true,
+        }
       }
     }
-  }
-})
+  };
+});
