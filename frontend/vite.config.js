104b7af312c72406aa19450717b0121ef6f2e7dc
ht45hamza <ht45hamzatariq@gmail.com>
2026-06-02 18:39:32 +0500

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,21 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load all environment variables (including those without VITE_ prefix)
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-    ],
+    plugins: [react()],
+
     server: {
       port: 5173,
       proxy: {
         '/api': {
-          target: `http://localhost:${env.BaseURL }`,
+          target: env.BaseURL,
           changeOrigin: true,
-        }
-      }
-    }
+          secure: true,
+        },
+      },
+    },
   };
 });
